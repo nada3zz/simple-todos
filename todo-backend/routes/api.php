3e2d5946f673d40b8b06c1ebb53f7dcd8808ed3a
nadaeezzdin <nadaeezzdin@gmail.com>
2025-04-10 21:49:21 +0200

    add routes

--- todo-backend/routes/api.php
+++ todo-backend/routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Task\TaskController;
+use App\Http\Controllers\category\CategoryController;
+use App\Http\Controllers\Auth\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+
+Route::post('/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    // Task Routes
+    Route::get('/tasks', [TaskController::class, 'getAllTasks']);
+    Route::post('/tasks', [TaskController::class, 'createTask']);
+    Route::get('/tasks/{task}', [TaskController::class, 'showTask']);
+    Route::put('/tasks/{task}', [TaskController::class, 'updateTask']);
+    Route::delete('/tasks/{task}', [TaskController::class, 'deleteTask']);
+    Route::post('/tasks/{task}/restore', [TaskController::class, 'restoreTask']);
+    Route::get('/tasks/deleted', [TaskController::class, 'getAllDeletedTasks']);
+
+    // Category Routes
+    Route::get('/categories', [CategoryController::class, 'getAllCategories']);
+    Route::post('/categories', [CategoryController::class, 'createCategory']);
+    Route::get('/categories/{category}', [CategoryController::class, 'showCategory']);
+    Route::put('/categories/{category}', [CategoryController::class, 'updateCategory']);
+    Route::delete('/categories/{category}', [CategoryController::class, 'deleteCategory']);
 });
