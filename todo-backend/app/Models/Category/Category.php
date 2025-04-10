be9d747e1333f22e12571a0fe75ff546b08355bf
nadaeezzdin <nadaeezzdin@gmail.com>
2025-04-10 21:47:04 +0200

    add category model

---
+++ todo-backend/app/Models/Category/Category.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models\Category;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Task\Task;
+use App\Models\User\User;
+
+class Category extends Model
+{
+    use HasFactory, SoftDeletes;
+
+
+    protected $fillable = [
+        'name',
+        'color',
+        'user_id',
+    ];
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
